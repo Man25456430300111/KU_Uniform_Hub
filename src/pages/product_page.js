0c6500c79d6ce28521157fe1371b4613317ff89b
6430301125 <aissariya.t@ku.th>
2024-10-04 22:53:23 +0700

    dd

--- src/pages/product_page.js
+++ src/pages/product_page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Product_page = () => {
   return (
     <div>
-      <h1>Welcome to the Product_page page!</h1>
+      <h1>Welcome to the Product_page page!gdfgdgg</h1>
     </div>
   );
 };
